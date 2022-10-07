41cf3d52b3634dc0a65a14dea4eb6967fe3a6c67
Liam Cariou <cariou.liam@orange.fr>
2022-10-07 11:16:02 +0200

    Update exo1.php

--- cours-php-poo/exo1.php
+++ cours-php-poo/exo1.php
@@ -2,19 +2,57 @@
 
 class Person {
 
-    public $name  ;
-    public $phoneNumber  ;
-    public $emailAdress  ;
+    public string $name  ;
+    public string $phoneNumber  ;
+    public string $emailAdress  ;
 
-    function purchaseParkingPass(){
+    public function purchaseParkingPass(){}
 
-    }
-
-    function __construct(){
-        
+    function __construct(string $monnom,string $tel,string $mail){
+        $this->name = $monnom;
+        $this->phoneNumber = $tel;
+        $this->emailAdress = $mail;
     }
 }
 
+
+
+
+class Student extends Person {
+
+    public int $studentNumber ;
+    public int $averageMark ; 
+
+    public function itEligibleToEnroll(string ):bool{
+
+    }
+
+    public function getSeminarsTaken():int{
+
+    }
+
+
+    
+}
+
+
+
+
+
+
+class Professor extends Person{
+ public int $salary ;
+ public int $staffNumber ; 
+ private int $yearsOfService ; 
+ public int $numberOfClasses ;
+
+}
+
+
+
+
+
+$personne = new Person("Liam", "Tel", "Mail");
 
 
 ?>
