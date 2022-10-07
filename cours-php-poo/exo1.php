92b5d4d6515057aadbbf02339ec60a4cbeb0094d
Liam Cariou <cariou.liam@orange.fr>
2022-10-07 10:25:02 +0200

    Update exo1.php

---
+++ cours-php-poo/exo1.php
@@ -0,0 +1,20 @@
+<?php
+
+class Person {
+
+    public $name  ;
+    public $phoneNumber  ;
+    public $emailAdress  ;
+
+    function purchaseParkingPass(){
+
+    }
+
+    function __construct(){
+        
+    }
+}
+
+
+
+?>
