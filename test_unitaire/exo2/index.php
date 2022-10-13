d10c205f5df5aaba0a7e6366232fd24085a12ed9
Liam Cariou <cariou.liam@orange.fr>
2022-10-13 14:35:13 +0200

    cours

---
+++ test_unitaire/exo2/index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+function add($nbr, $nbr2){
+    $cal = ($nbr + $nbr2);
+    return $cal;
+}
+
+function sous($nbr, $nbr2){
+    $cal = ($nbr - $nbr2);
+    return $cal;
+}
+function div($nbr, $nbr2){
+    $cal = ($nbr / $nbr2);
+    return $cal;
+}
+function mul($nbr, $nbr2){
+    $cal = ($nbr * $nbr2);
+    return $cal;
+}
+
+echo add(7,2) . PHP_EOL;
+echo sous(10, 10) . PHP_EOL;
+echo div(3, 6) . PHP_EOL;
+echo mul(1, 2) . PHP_EOL;
+
+
+
+
+
+
+
+?>
---
+++ test_unitaire/exo2/test.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo calcul(78, 96) . PHP_EOL;
+//check(42, add(21, 21));
+//echo PHP_EOL;
+
+
+?>
